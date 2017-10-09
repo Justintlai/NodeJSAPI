911679fe561c39ac650a62e25601071971d2c5cb
Justin <hello@justintlai.com>
2017-10-09 11:59:07 +0100

    Updated Routes for API

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
 
 const port = 8000;
 
-require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err);
+  require('./app/routes')(app, database);
+});
 
 app.listen(port, () => {
   console.log('We are LIVE on ' + port);
